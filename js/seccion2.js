07dbb875e1a7382a43a570ff5f1e2e0336e718d6
yrondon757 <yrondon757@gmail.com>
2024-09-23 00:17:09 -0400

    1

--- js/seccion2.js
+++ js/seccion2.js
@@ -596,3 +596,221 @@ console.log(player3.nombre);
 // Usar el metodo SET
 player3.nombre = "Jose";
 console.log(player3.nombre);
+
+
+
+
+// ------------------------- SUBCLASE (HERENCIA)
+/*
+    La herencia tambien suele ser llamada POLIMORFISMO, ya que nos permite crear objetos que comparten atributos y metodos en una clase padre, pero que a su vez puede tener atributos y metodos propios.
+*/
+
+class Enemigo extends Player{
+    constructor(nombre, color, fuerza){
+        super(nombre, color); // Heredamos los atributos de la clase "Player"
+        this._fuerza = fuerza;  
+    }
+
+    get fuerza(){
+        return this._fuerza;
+    }
+
+    set fuerza(nuevafuerza){
+        this._fuerza = nuevafuerza;
+    }
+}
+
+let enemigo1 = new Enemigo("Bowser","Amarillo",900);
+console.log(enemigo1);
+enemigo1.saludar(); // Metodo que hereda de la clase padre
+console.log(enemigo1.fuerza);
+
+
+
+// Crear una clase llamada calculadora, que no tenga funcion constructora, y que tenga los siguientes metodos:
+// Sumar, Restar, Multiplicar y Dividir.
+
+class Calculadora{
+    sumar(a,b){
+        return a + b;
+    }
+    restar(a,b){
+        return a - b;
+    }
+    multiplicar(a,b){
+        return a * b;
+    }
+    dividir(a,b){
+        if(b === 0){
+            return "No es posible";
+        }else{
+            return a / b;
+        }
+    }
+}
+
+let calculadora1 = new Calculadora();
+console.log(calculadora1.sumar(50,50));
+console.log(calculadora1.restar(50,50));
+console.log(calculadora1.multiplicar(50,50));
+console.log(calculadora1.dividir(50,50));
+console.log(calculadora1.dividir(50,0));
+
+
+
+
+// ------------------------- BUCLES (LOOPS) (CICLOS)
+
+// ------------ FOR
+/*
+for()
+1. let i = 0 ----> Declaracion de la variable (Contadora)
+
+Nota: Cada parte va separada por ;
+
+2. i < 10; ----> Esta es la condicion que se verifica en cada iteracion del ciclo.
+
+3. i++ ----> i = i + 1 ----> Despues de cada iteracion, la variable se incrementa.
+
+Como se escribe un ciclo for:
+for(let i = 0; i < 10; i++){
+// Aqui colocamos la logica que deseemos que se ejecute cada vez que el ciclo se repita
+}
+*/
+
+for(let i = 1; i <= 10; i++){
+    //console.log("Hola mundo" + i);
+    console.log(`Hola mundo ${i}`); // Interpolar ``. Se puede escribir codigo Javascript.
+};
+
+// for con array
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i]);
+}
+
+// Crear un ciclo que imprima solo los numeros pares del 0 al 100.
+for(let i = 0; i <=100; i += 2){ // Se puede hacer de las dos formas.
+    /*if(i % 2 === 0){
+        console.log(`${i} es un numero par`);
+    }*/
+    console.log(`${i} es un numero par`);
+}
+
+
+
+// ------------------- FOR IN ----> DEVUELVE LA POSICION DE LOS ELEMENTOS
+// Para arrays
+for(let i in frutas){
+    console.log(i);
+    console.log(frutas[i]);
+}
+
+// En objetos
+let bicicleta = {
+    marca: "Mountain Bike",
+    rodado: 26,
+    color: "Negro",
+    cambios:21
+};
+
+for(let i in bicicleta){
+    // console.log(i); // De esta forma devuelve las propiedades; marca, rodado, color, cambios.
+    // console.log(bicicleta[i]); // De esta forma devuelve las valores; Mountain Bike, 26, Negro, 21
+    console.log(`${i} : ${bicicleta[i]}`);
+}
+
+
+// Imprime en una sola variable, el nombre, apellido y edad del siguiente objeto:
+let persona = {
+    nombre: "Jose",
+    apellido: "Perez",
+    edad: 30
+};
+
+let acumulador = "";
+
+for(let propiedad in persona){
+    acumulador += `${persona[propiedad]} `;
+}
+
+console.log(acumulador);
+
+
+
+
+// ----------------------- FOR OF ----> DEVUELVE EL ELEMENTO DIRECTAMENTE Y NO LA POSICION
+for(let fruta of frutas){
+    console.log(fruta);
+}
+
+/*for(let i of persona){ // El FOR OF solo funciona con arrays, no funciona con objetos.
+    console.log(i);
+}*/
+
+// ---------------- CICLO WHILE
+let contador = 0;
+
+while(contador <= 10){
+    console.log(contador);
+    contador++; // contador = contador + 1
+}
+
+// ----------------- CICLO DO WHILE
+// SE EJECUTA SIEMPRE AL MENOS UNA VEZ
+let contador2 = 0;
+do{
+    console.log(contador2);
+    contador2++;
+}while(contador2 <= 10);
+
+
+
+// EJERCICIOS
+let compras = [["carne molida","carne mechada"],["papas","batatas","cebollas"],["jamon","queso","salame"]];
+
+console.log(compras[1][1]);
+// Imprime cada uno de los elementos dentro de los arrays del array de compras (con for).
+
+for (let i in compras){
+    for(let j in compras[i]){
+        console.log(compras[i][j]);
+    }
+}
+
+/* EJERCICIOS 
+
+1) Crea un juego en el que el programa genera un numero aleatorio del 1 al 100, y luego le pide al usuario ese numero. El bucle "WHILE" debe continuar ejecutandose hasta que el usuario adivine correctamente el numero. (Investigar Math.random)
+
+2) Crea un Objeto que contenga informacion sobre libros, donde cada propiedad es el titulo de un libro y su valor es un objeto con detalles adicionales (autor,año de publicacion u otra cosa). Utilizar un bucle "FOR IN" para imprimir el titulo y el autor.
+
+3) Crea un array de objetos que presenten personas, donde cada objeto tenga propiedades como (nombre,edad y ciudad). Utilizar un bucle "FOR OF" para imprimir el nombre y la ciudad de cada persona.
+
+4) Crea un programa que elija una palabra al azar de una lista predefinida y le pida al usuario que adivine la palabra letra por letra. Utilizar bucle "DO WHILE", para continuar solicitando letras hasta que el usuario adivine la palabra o haya cometido un numero maximo de intentos. (Investigar Math.random).
+
+5) Crea una clase llamada "Persona" que tenga un constructor que reciba dos parametros (nombre y edad). La clase debe tener un metodo "saludar" y que imprima un mensaje de saludo con el nombre y la edad de la persona.
+
+6) Crea una clase llamada "Estudiante" que herede de la clase "Persona" del ejercicio anterior. La clase "Estudiante" debe tener un constructor adicional que reciba un parametro (curso). Agregar un metodo "presentar" a la clase "Estudiante" que incluya la informacion del nombre,edad y curso.
+
+7) Crea una funcion que tome una cadena de texto y dos palabras como argumentos (parametros), y reemplace todas las ocurrencias de la primera palabra con la segunda palabra.
+
+8) Escribe una funcion que tome un array de objetos, donde cada objeto tiene una propiedad "edad" y filtre los objetos que tienen una edad mayor o igual a 18.
+
+9) Dado el siguiente array [{nombre: "Luis", edad: 33, hobbies: ["videojuegos, "musica, "cine"]}, {nombre: "Juanito", edad: 30}, {nombre: "Pedro", edad: 20, hobbies: ["deportes", "leer"]}]. Recorre e imprime cada uno de los valores de cada uno de los elementos.
+
+10) Agrega un ultimo elemento al array anterior con el nombre de "Maria" y la edad de 25 años
+
+11) Encuentra el elemento con el nombre de "Pedro" y elimina el elemento hobbies.
+
+12) Encuentra el elemento con el nombre de "Juanito" y agrega un elemento hobbies con el valor de "diseñar".
+
+13) Crear una clase llamada "Inventario" que se encargará de gestionar un inventario de productos:
+ * La clase "Inventario" debe tener un constructor que inicialice un arreglo vacío llamado productos.
+ * Debe haber un método llamado agregarProducto que acepte tres parámetros (nombre, precio, y cantidad). Este método deberá agregar un objeto producto al arreglo productos con los datos proporcionados.
+ * Debe existir un método llamado calcularValorTotal que calcule y devuelva el valor total del inventario multiplicando el precio por la cantidad para cada producto en el arreglo y sumándolos.
+ * Debe haber un método llamado listarProductos que recorra el arreglo productos y muestre en la consola los detalles de cada producto (nombre, precio y cantidad).
+ * Crea una instancia de la clase "Inventario" llamada miInventario.
+ * Agrega al menos tres productos a miInventario utilizando el método agregarProducto
+ * Llama al método listarProductos en miInventario para mostrar la lista de productos en la consola.
+ * Llama al método calcularValorTotal en miInventario y muestra el valor total del inventario en la consola.
+
+*/
