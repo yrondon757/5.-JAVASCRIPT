8e507b49247ee0f065ef82db5d6e56eecde83145
yrondon757 <yrondon757@gmail.com>
2024-09-23 00:19:10 -0400

    1

---
+++ js/ejercicios.js
@@ -0,0 +1,37 @@
+/* EJERCICIOS 
+
+1) Crea un juego en el que el programa genera un numero aleatorio del 1 al 100, y luego le pide al usuario ese numero. El bucle "WHILE" debe continuar ejecutandose hasta que el usuario adivine correctamente el numero. (Investigar Math.random)
+
+2) Crea un Objeto que contenga informacion sobre libros, donde cada propiedad es el titulo de un libro y su valor es un objeto con detalles adicionales (autor,año de publicacion u otra cosa). Utilizar un bucle "FOR IN" para imprimir el titulo y el autor.
+
+3) Crea un array de objetos que presenten personas, donde cada objeto tenga propiedades como (nombre,edad y ciudad). Utilizar un bucle "FOR OF" para imprimir el nombre y la ciudad de cada persona.
+
+4) Crea un programa que elija una palabra al azar de una lista predefinida y le pida al usuario que adivine la palabra letra por letra. Utilizar bucle "DO WHILE", para continuar solicitando letras hasta que el usuario adivine la palabra o haya cometido un numero maximo de intentos. (Investigar Math.random).
+
+5) Crea una clase llamada "Persona" que tenga un constructor que reciba dos parametros (nombre y edad). La clase debe tener un metodo "saludar" y que imprima un mensaje de saludo con el nombre y la edad de la persona.
+
+6) Crea una clase llamada "Estudiante" que herede de la clase "Persona" del ejercicio anterior. La clase "Estudiante" debe tener un constructor adicional que reciba un parametro (curso). Agregar un metodo "presentar" a la clase "Estudiante" que incluya la informacion del nombre,edad y curso.
+
+7) Crea una funcion que tome una cadena de texto y dos palabras como argumentos (parametros), y reemplace todas las ocurrencias de la primera palabra con la segunda palabra.
+
+8) Escribe una funcion que tome un array de objetos, donde cada objeto tiene una propiedad "edad" y filtre los objetos que tienen una edad mayor o igual a 18.
+
+9) Dado el siguiente array [{nombre: "Luis", edad: 33, hobbies: ["videojuegos, "musica, "cine"]}, {nombre: "Juanito", edad: 30}, {nombre: "Pedro", edad: 20, hobbies: ["deportes", "leer"]}]. Recorre e imprime cada uno de los valores de cada uno de los elementos.
+
+10) Agrega un ultimo elemento al array anterior con el nombre de "Maria" y la edad de 25 años
+
+11) Encuentra el elemento con el nombre de "Pedro" y elimina el elemento hobbies.
+
+12) Encuentra el elemento con el nombre de "Juanito" y agrega un elemento hobbies con el valor de "diseñar".
+
+13) Crear una clase llamada "Inventario" que se encargará de gestionar un inventario de productos:
+ * La clase "Inventario" debe tener un constructor que inicialice un arreglo vacío llamado productos.
+ * Debe haber un método llamado agregarProducto que acepte tres parámetros (nombre, precio, y cantidad). Este método deberá agregar un objeto producto al arreglo productos con los datos proporcionados.
+ * Debe existir un método llamado calcularValorTotal que calcule y devuelva el valor total del inventario multiplicando el precio por la cantidad para cada producto en el arreglo y sumándolos.
+ * Debe haber un método llamado listarProductos que recorra el arreglo productos y muestre en la consola los detalles de cada producto (nombre, precio y cantidad).
+ * Crea una instancia de la clase "Inventario" llamada miInventario.
+ * Agrega al menos tres productos a miInventario utilizando el método agregarProducto
+ * Llama al método listarProductos en miInventario para mostrar la lista de productos en la consola.
+ * Llama al método calcularValorTotal en miInventario y muestra el valor total del inventario en la consola.
+
+*/
